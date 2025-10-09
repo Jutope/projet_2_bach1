38d9e30289183c5ac35edd494bb18f5f01277f94
Jutope <quentin.reynaud21@gmail.com>
2025-10-09 16:53:31 +0200

    Mise a jour
    je sais plus ce que j'ai fait mais c'est la dernière version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("galerie-btn");
   btn.addEventListener("click", () => {
-    window.location.href = "galerie.html";
+    window.location.href = "../galerie/galerie.html";
   });
 });
